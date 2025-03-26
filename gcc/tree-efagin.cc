e374afe088966ce62877a71d88da6741fa13580c
emily fagin <emilyfagin@gmail.com>
2025-03-26 17:17:08 -0400

    added bb & gimple iteration

--- gcc/tree-efagin.cc
+++ gcc/tree-efagin.cc
@@ -1,7 +1,9 @@
-/* Language independent return value optimizations
+/* Test pass
+   Language independent return value optimizations
    Copyright (C) 2004-2025 Free Software Foundation, Inc.
 
-This file is part of GCC.
+
+This file is part of GCC
 
 GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,14 +32,19 @@ along with GCC; see the file COPYING3.  If not see
 #include "gimple-iterator.h"
 #include "gimple-walk.h"
 #include "internal-fn.h"
+#include "gimple-pretty-print.h"
 
 // Added  headers:
+
+#include "gimple.h"
 #include "gimple-ssa.h"
 #include "cgraph.h"
 #include "attribs.h"
 #include "pretty-print.h"
 #include "tree-inline.h"
 #include "intl.h"
+#include "basic-block.h"
+
 
 // for dump_printf
 #include "tree-pretty-print.h"
@@ -72,9 +79,11 @@ public:
   /* opt_pass methods: (--> all the time, so return 1)*/
   bool gate (function *) final override { return 1; }
 
-  unsigned int execute (function *) final override {
+  unsigned int execute (function *fun) final override {
+    basic_block bb;
     struct cgraph_node *node;
-    int func_cnt = 0;
+
+    int func_cnt = 0, bb_cnt = 0, stmt_cnt = 0;
 
     // macro for iterating through all functions
     FOR_EACH_FUNCTION (node) {
@@ -82,18 +91,37 @@ public:
         fprintf(dump_file, "=== Function %d Name '%s' ===\n", ++func_cnt, node->name());
       }
     }
+
+    // macro for iterating through all basic blocks
+    FOR_EACH_BB_FN(bb, fun) {
+      bb_cnt++;
+      if (dump_file) {
+        fprintf(dump_file, "=== Basic block count: %d ===\n", bb_cnt);
+      }
+      for (gimple_stmt_iterator gsi = gsi_start_bb (bb); !gsi_end_p (gsi); gsi_next (&gsi)) {
+        gimple *g = gsi_stmt (gsi);
+        stmt_cnt++;
+        if (dump_file) {
+          fprintf (dump_file, "----- Statement Count: %d ------\n", stmt_cnt);
+          print_gimple_stmt (dump_file, g, 0, TDF_VOPS|TDF_MEMSYMS);
+        } 
+      }
+    }
+
+
     if (dump_file) {
       fprintf(dump_file, "\n\n## End efagin diagnost, strt regular dump of curr gimple###\n\n");
     }
     return 0;
-  };
+  }
 
 }; // class pass_efagin
 
-}; // anon namespace
+} // anon namespace
 
 gimple_opt_pass *
 make_pass_efagin (gcc::context *ctxt)
 {
   return new pass_efagin (ctxt);
 }
+
