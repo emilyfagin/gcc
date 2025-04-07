033c495913d09723d807dde0aab628ae83ddcb8e
Emily Fagin <emilyfagin@gmail.com>
2025-04-07 19:47:11 -0400

    Add signature comparison

--- gcc/tree-efagin.cc
+++ gcc/tree-efagin.cc
@@ -1,5 +1,5 @@
 /* efagin pass - identifies function clones
-   Emily Fagin - Seneca Polytechnic College
+   Emily Fagin - Seneca Polytechnic College (2025)
 
 This file is part of GCC
 
@@ -20,6 +20,7 @@ along with GCC; see the file COPYING3.  If not see
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <stdlib.h>
 #include "config.h"
 #include "system.h"
@@ -34,6 +35,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "cgraph.h"
 #include "function.h"
 #include "basic-block.h"
+#include "gimple-pretty-print.h"
 
 namespace {
 
@@ -51,63 +53,108 @@ const pass_data pass_data_efagin =
   0,              /* todo_flags_finish */
 };
 
-/* Currently, this pass detects function clones during compilation.
-   It identifies variants using the base name of the function, and outputs the analysis.
+// funcData holds the function's full name, as well as signature.
+struct funcData { std::string full_name; std::string signature; };
+
+/* 
+   Currently, this pass generates a function signature based on 
+   gimple statements, and other metrics. It detects function 
+   clones using the base name of the function, and compares their 
+   signature, outputting PRUNE or NOPRUNE based on analysis.
 */
 class pass_efagin : public gimple_opt_pass
 {
 public:
-  pass_efagin (gcc::context *ctxt)
-    : gimple_opt_pass (pass_data_efagin, ctxt)
-  {}
-
-  /* gate function: always execute */
+  pass_efagin (gcc::context *ctxt): gimple_opt_pass 
+    (pass_data_efagin, ctxt){}
   bool gate (function *) final override { return 1; }
 
   unsigned int execute (function *) final override;
-
-private:
+	
+private:  
   /* Stores function names to identify clones*/
-  static std::unordered_map<std::string, std::vector<std::string>> function_clones;
+  static std::unordered_map<std::string, std::vector<funcData>> 
+    fun_map;
 
+  std::string get_signature(function* fun);
 };
 
 // Initialize static member
-std::unordered_map<std::string, std::vector<std::string>> pass_efagin::function_clones;
+std::unordered_map<std::string, std::vector<funcData>> 
+  pass_efagin::fun_map;
+
+// Generate a signature for a function
+std::string pass_efagin::get_signature(function* fun) {
+  std::stringstream signature;
+
+  basic_block bb;
+  int num_gimple_stmts = 0;
+
+  FOR_EACH_BB_FN(bb, fun) {
+    // iterate through all gimple statments in block
+    for (gimple_stmt_iterator gsi = gsi_start_bb(bb); 
+      !gsi_end_p(gsi); gsi_next(&gsi)) {
+        
+      // Record statement type with a space prefix
+      signature << gimple_code(gsi_stmt(gsi)) << " ";
+      num_gimple_stmts++;
+    }
+  }
+    
+  signature 
+    << "\nBB\t" << n_basic_blocks_for_fn(fun) 
+    << "\nEG\t" << n_edges_for_fn(fun) 
+    << "\nGMP-S\t" << num_gimple_stmts;
+  
+  return signature.str();
+}
 
 unsigned int pass_efagin::execute(function* fun)
 {
   // If dump file not enabled, exit
-  if (!dump_file || !fun)
+  if (!dump_file || !fun )
     return 0;
 
   // Get the full name of the function
   std::string name = IDENTIFIER_POINTER(DECL_NAME(fun->decl));
-  fprintf(dump_file, "-------------Examining Func: %s--------\n", name.c_str());
-
+  fprintf(dump_file, "--------Examining Func: %s-----\n", 
+    name.c_str());
+	
   // Skip resolver functions
   if (name.find(".resolver") != std::string::npos) {
-    fprintf(dump_file, "-------------End of Diagnostic (resolver function)-------------\n\n");
+    fprintf(dump_file, "---End of Diagnostic (resolver)---\n");
     return 0;
   }
 
   // Extract base function name (remove everything after first dot)
-  std::string base_name = name.find_first_of(".") != std::string::npos
-    ? name.substr(0, name.find_first_of("."))
+  std::string base_name = name.find_first_of(".") 
+    != std::string::npos 
+    ? name.substr(0, name.find_first_of(".")) 
     : name;
 
-  auto it = function_clones.find(base_name);
+  funcData fdata = {name, get_signature(fun)};
+  fprintf(dump_file, "SIGNATURE:\n%s\n", fdata.signature.c_str());
 
+  
+  auto it = fun_map.find(base_name);
+ 
   // If this base name already has registered clones, print them
-  if (it != function_clones.end()) {
-    for (const auto& variant : it->second)
-      fprintf(dump_file, "CLONE IDENTIFIED: %s\n", variant.c_str());
-    fprintf(dump_file, "CURRENT: %s\n\n", name.c_str());
+  if (it != fun_map.end()) {
+    for (auto& variant : it->second) {
+      fprintf(dump_file, "CLONE: %s\nCLONE SIGNATURE:\n%s\n", 
+        variant.full_name.c_str(), variant.signature.c_str());
+      
+      if (fdata.signature == variant.signature) {
+        fprintf(dump_file, "[%s] = PRUNE\n", base_name.c_str());
+      } else {
+        fprintf(dump_file, "[%s] = NOPRUNE\n", base_name.c_str());
+      }
+    }
   }
-
-  // Add this function name to the list of variants for the base function
-  function_clones[base_name].push_back(name);
-  fprintf(dump_file, "-------------End of Diagnostic-------------\n\n");
+  
+  // Add function data to the clones map
+  fun_map[base_name].push_back(fdata);
+  fprintf(dump_file, "--------End of Diagnostic---\n\n");
 
   return 0;
 }
